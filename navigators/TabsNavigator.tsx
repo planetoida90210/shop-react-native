fb8c12b6e28acd19ccaedcdeeee814956eb3036e
Michał Bryk <galeriakoloru90210@gmail.com>
2023-04-17 23:50:30 +0200

    tab navigator

---
+++ navigators/TabsNavigator.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export type TabsStackParamList = {
+  Home: undefined;
+};
+
+const TabsNavigator = () => {
+  return (
+    <View>
+      <Text>TabsNavigator</Text>
+    </View>
+  );
+};
+
+export default TabsNavigator;
