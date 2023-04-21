4d236f438d181d396099952d2bc450d0a4f9a7e8
Michał Bryk <galeriakoloru90210@gmail.com>
2023-04-21 07:44:46 +0200

    add avatars to naviagtion

--- navigators/TabsNavigator.tsx
+++ navigators/TabsNavigator.tsx
@@ -1,6 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icons from "@expo/vector-icons/MaterialIcons";
+
+//external imports
 import { CartScreen, HomeScreen, PaymentScreen, ProfileScreen } from "../screens";
 
 export type TabsStackParamList = {
@@ -15,10 +18,46 @@ const TabStack = createBottomTabNavigator<TabsStackParamList>();
 const TabsNavigator = () => {
   return (
     <TabStack.Navigator screenOptions={{ tabBarShowLabel: false }}>
-      <TabStack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-      <TabStack.Screen name="Cart" component={CartScreen} options={{ headerShown: false }} />
-      <TabStack.Screen name="Payment" component={PaymentScreen} options={{ headerShown: false }} />
-      <TabStack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+      <TabStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon(props) {
+            return <Icons name="home" {...props} />;
+          },
+        }}
+      />
+      <TabStack.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon(props) {
+            return <Icons name="shopping-cart" {...props} />;
+          },
+        }}
+      />
+      <TabStack.Screen
+        name="Payment"
+        component={PaymentScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon(props) {
+            return <Icons name="account-balance-wallet" {...props} />;
+          },
+        }}
+      />
+      <TabStack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon(props) {
+            return <Icons name="person" {...props} />;
+          },
+        }}
+      />
     </TabStack.Navigator>
   );
 };
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,19 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, ScrollView, Image } from "react-native";
 import React from "react";
+import { ProfileUser } from "../assets/images";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
+    <ScrollView>
+      <SafeAreaView>
+        <View className="flex-row p-6 items-center gap-2">
+          <Image source={ProfileUser} className="w-[52px] aspect-square rounded-full" resizeMode="cover" />
+          <View className="flex-1">
+            <Text>Hi, Bolek </Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
